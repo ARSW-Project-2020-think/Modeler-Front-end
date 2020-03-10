3645a37fea32bcf762c55abecb5a62163348c76c
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-03-10 12:01:38 -0500

    Listo

--- js/apiclient.js
+++ js/apiclient.js
@@ -4,14 +4,28 @@ var apiclient = (function () {
     return {
         registrarse: function(newUsuario) {
             alert("ENTRO REGISTRER");
-            var register = $.ajax({type: "POST", url:"https://class-modeler.herokuapp.com/user",
+            var register = $.post({url:"https://class-modeler.herokuapp.com/user",
                     data: JSON.stringify(newUsuario), contentType: "application/JSON"});
             register.then (function(){
+                alert("correct ");
                 location.href = "mainPage.html";
             }, function(){
                 alert("Usuario o correo electrónico inválidos");
-                location.href = "index.html";
+                //location.href = "index.html";
             });
+        },login:function(email,password,callback){
+            var json = {username:email};
+            console.log(JSON.stringify(json));
+            var promise = $.get({
+                url:"https://class-modeler.herokuapp.com/user/login",
+                data: JSON.stringify(json),contentType: "application/json"
+            });
+            promise.then(function(data){
+                callback(email,password,data.token);
+            },function(data){
+                alert("Error en credenciales"+data);
+            });
+
         }
     }
 
--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 var app = (function () {
-
+    var createSession = function(email,password,token){
+        sessionStorage.setItem("email", email);
+        sessionStorage.setItem("password", password);
+        sessionStorage.setItem("token", token);
+        location.href = "mainPage.html";
+    }
     return {
-        registrarse: function(user, email, password) {
-            console.log("Primero");
-            alert("HOLA1");
+        registrarse: function(user, email, password,event) {
+            event.preventDefault();
+            //console.log("Primero");
+            //alert("HOLA1");
             var newUsuario = {username:user, correo: email, password: password};
             apiclient.registrarse(newUsuario);
-            alert("HOLA2");
+            //alert("HOLA2");
+        },login:function(email,password,event){
+            event.preventDefault();
+            apiclient.login(email,password,createSession);
         }
     }
 
