201b74db2a7d162c43c01e4ab1e0f0adadbf8718
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-04-03 12:11:28 -0500

    Concurrente

--- src/main/resources/static/js/drawer.js
+++ src/main/resources/static/js/drawer.js
@@ -1,34 +1,10 @@
 var draw = (function(){
 	var clases = [];
+	var cont = [];
 	var stompClient = null;
-	var idModelo = null; 
+	var idModelo = null;
+	var selected = null;
 	var url = "https://class-modeler.herokuapp.com";
-	var createRectangle = function(error,rectangulo){
-		if(error!=null){
-			alert("No fue posible registrar la nueva clase");
-			return ;
-		}
-		var canv = $("#dib");
-		var clase = $("<div style='width:"+rectangulo.ancho+"px; height:"+rectangulo.alto+"px; background:black; margin:0px; color:white; text-align:center; padding:0px;'></div>");
-		clase.css("position","relative");
-		clase.text(rectangulo.nombre);
-		clase.css("left",(rectangulo.x-11)+"px");
-    	clase.css("top",rectangulo.y-(50*clases.length)+"px");
-    	clase.click(function(ev){
-    		ev.stopPropagation();
-    	});
-    	clase.draggable({containment:"parent",
-    		drag:function(drev){
-    			//Send to Socket
-    			var y = $(this).css("top");
-    			var x = $(this).css("left");
-    			console.log("X: "+x);
-    			console.log("Y: "+y);
-    			}
-    	});
-    	clases.push(rectangulo);
-    	canv.append(clase);
-	};
 	var drawClases = function(err,data){
 		if(err!=null){
 			console.log("No se han podido cargar las clases, revise su conexion o contactese con el administrador");
@@ -41,7 +17,7 @@ var draw = (function(){
 			loadSocket(data.id);
 		}
 		data.rectangulos.forEach(function(rectangulo){
-			var clase = $("<div style='width:"+rectangulo.ancho+"px; height:"+rectangulo.alto+"px; background:black; margin:0px; color:white; text-align:center; padding:0px;'></div>");
+			var clase = $("<div style='width:"+rectangulo.ancho+"px; height:"+rectangulo.alto+"px; background:black; margin:0px; color:white; text-align:center; padding:0px;' id='"+clases.length+"'></div>");
 			clase.css("position","relative");
 			clase.text(rectangulo.nombre);
 			clase.css("left",(rectangulo.x-11)+"px");
@@ -51,14 +27,23 @@ var draw = (function(){
 	    	});
 	    	clase.draggable({containment:"parent",
 	    		drag:function(drev){
-	    			//Send to Socket
-	    			var y = $(this).css("top");
-	    			var x = $(this).css("left");
-	    			console.log("X: "+x);
-	    			console.log("Y: "+y);
+	    				if(selected==null){
+	    					selected = rectangulo;
+	    				}
+		    			var y = parseInt($(this).css("top").substring(0,$(this).css("top").length-2))+50*parseInt($(this).attr("id"));
+		    			var x = parseInt($(this).css("left").substring(0,$(this).css("left").length-2))+11;
+		    			rectangulo.x = x;
+		    			rectangulo.y = y;
+		    			stompClient.send('/app/updaterectangle.'+idModelo,{},JSON.stringify(rectangulo));
 	    			}
 	    	});
+	    	clase.mouseup(function() {
+	    		  if(selected!= null && selected.id==rectangulo.id){
+	    			  selected = null;
+	    		  }
+	    		});
 	    	clases.push(rectangulo);
+	    	cont.push(clase);
 	    	canv.append(clase);
 			
 		});
@@ -74,6 +59,24 @@ var draw = (function(){
             	alert("recibio algo");
             	drawClases(null,{rectangulos:[JSON.parse(eventbody.body)]});
             });
+            stompClient.subscribe('/shape/updaterectangle.'+idModelo, function (eventbody) {
+            	var rect = JSON.parse(eventbody.body);
+            	//alert("recibio update");
+            	if(selected!=null && selected.id==rect.id){
+            		return;
+            	};
+            	console.log(rect);
+            	for(var i=0;i<cont.length;i++){
+            		if(clases[i].id==rect.id && clases[i].x != rect.x && clases[i].y != rect.y){
+            			clases[i].x = rect.x;
+            			clases[i].y = rect.y;
+            			cont[i].css("left", (clases[i].x-11)+"px");
+            			cont[i].css("top",(clases[i].y-(50*parseInt(cont[i].attr("id"))))+"px");
+            			console.log("update location");
+            			console.log(cont[i]);
+            		}
+            	}
+            });
         });
 	};
 	return {
@@ -85,7 +88,6 @@ var draw = (function(){
         		alert("Debes dar nombre a una clase para crearla");
         		return;
         	}
-        	var clase = $("<div style='width:200px; height:50px; background:black; margin:0px; color:white; text-align:center; padding:0px;'></div>");
         	var x = (event.pageX-275+11);
         	var y = event.pageY;
         	var rectangulo = {"x":x,"y":y,"ancho":200,"alto":50,"nombre":val};
