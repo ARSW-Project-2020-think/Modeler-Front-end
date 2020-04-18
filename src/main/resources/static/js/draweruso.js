c7fe1d8dc5793156fc84634fe85abfff08688ee4
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-04-18 14:27:29 -0500

    app

--- src/main/resources/static/js/draweruso.js
+++ src/main/resources/static/js/draweruso.js
@@ -1,7 +1,8 @@
 var drawer = (function(){
-	
+	var idModelo = null;
 	var stompClient = null;
-	
+	var ovalos = [];
+	var url = "https://class-modeler.herokuapp.com";
 	var drawComponentes = function(err,data){
 		if(err!=null){
 			alert("Hubo un error");
@@ -11,11 +12,35 @@ var drawer = (function(){
 			loadSocket(data.id);
 		}
 		console.log(data);
+		drawOvals(data.componentes);
+	};
+	var drawOvals = function(ovalos){
+		ovalos.forEach(function(ovalo){
+			console.log(ovalo);
+		});
 	};
 	var loadSocket = function(id){
-		
+		idModelo = id;
+		var socket = new SockJS(url+'/modeler');
+        stompClient = Stomp.over(socket);
+        stompClient.connect({}, function (frame) {
+            console.log('Connected: ' + frame);
+            stompClient.subscribe('/shape/newcomponent.'+idModelo, function (eventbody) {
+            	drawComponentes(null,{componentes:[JSON.parse(eventbody.body)]});
+            });
+        });
 	}
 	return{
+		draw:function(event){
+			console.log(event);
+			var text = $("#novalo").val();
+			console.log(text);
+			var x = (event.pageX-275+11);
+        	var y = event.pageY;
+			var ovalo = {"@type":"Ovalo","nombre":text,"x":x,"y":y};
+			stompClient.send("/app/newcomponent."+idModelo,{},JSON.stringify(ovalo));
+			$("#novalo").val("");
+		},
 		getComponents:function(){
 			var url = new URL(document.URL);
         	var params = url.searchParams;
